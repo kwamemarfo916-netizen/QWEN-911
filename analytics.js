2539c6d93f0b82501f3ae61b8cd95078df295641
kwamemarfo916-netizen <kwamemarfo916@gmail.com>
2026-03-07 10:22:02 +0000

    analytics.js

--- analytics.js
+++ analytics.js
@@ -3,7 +3,7 @@ import { createClient } from
 
 const supabase = createClient(
 "https://qagawtgfpfupobwuotvw.supabase.co",
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhZ2F3dGdmcGZ1cG9id3VvdHZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMDIzNzYsImV4cCI6MjA4Nzg"
+"YOUR_ANON_KEY"
 );
 
 /* GLOBAL TRACK FUNCTION */
@@ -13,18 +13,23 @@ metadata = {}
 ){
 
 try{
+
 await supabase
 .from("analytics_events")
 .insert([
 {
 username: localStorage.getItem("username") || "guest",
-event_type,
-metadata,
-created_at: new Date().toISOString()
+event_type: event_type,
+metadata: metadata,
+created_at: new Date().toISOString(),
+page: location.pathname
 }
 ]);
+
 }catch(err){
+
 console.log("Tracking error:",err);
+
 }
 
 };
